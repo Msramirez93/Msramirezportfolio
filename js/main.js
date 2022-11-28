293959c6fa65bf4adceaf23b1076f815a3249f24
Msramirez93 <melany.salome.ramirez@gmail.com>
2022-11-28 15:30:51 -0300

    week 11

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ const links = [
     url: "week1/index.html"
     }
 ]
+document.querySelector("#Year").innerHTML=document.lastModified;
+document.querySelector('#lastmod').textContent = document.lastModified;
+
+/*const todaysDate = new Date();
+document.querySelector(`#idName`).textContent = `${todaysDate.getFullYear()}`;*/
